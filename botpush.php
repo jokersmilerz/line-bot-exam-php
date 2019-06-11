a4858f32f149377e809f5a2b86a64dd2ada9df15
jokersmilerz <51678222+jokersmilerz@users.noreply.github.com>
2019-06-12 01:11:24 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -18,7 +18,21 @@ $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
-
+if ( sizeof($request_array['events']) > 0 ) {
+   foreach ($request_array['events'] as $event) {
+      
+      $reply_message = '';
+      $reply_token = $event['replyToken'];
+      $text = $event['message']['text'];
+      $data = [
+         'replyToken' => $reply_token,
+         'messages' => [['type' => 'text', 'text' => $text ]]
+      ];
+      $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+      $send_result = send_reply_message($API_URL.'/reply',      $POST_HEADER, $post_body);
+      echo "Result: ".$send_result."\r\n";
+    }
+}
 
 
 
